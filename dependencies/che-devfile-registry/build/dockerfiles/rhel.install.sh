1692d20200d655176fe8bc81460d7e31bb07f8a3
Arvin Bhatnagar <abhatnag@us.ibm.com>
2020-08-27 18:42:19 -0400

    Update to CRW 2.3

--- dependencies/che-devfile-registry/build/dockerfiles/rhel.install.sh
+++ dependencies/che-devfile-registry/build/dockerfiles/rhel.install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2019 Red Hat, Inc.
+# Copyright (c) 2018-2020 Red Hat, Inc.
 # Copyright IBM Corporation 2020
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
