c16d3c99a8c6d701b40be5812461ed12ef3ba94f
Arvin Bhatnagar <abhatnag@us.ibm.com>
2021-01-15 12:35:24 -0500

    Update for plugin registry
    Update for manual build

--- dependencies/che-plugin-registry/build/dockerfiles/rhel.install.sh
+++ dependencies/che-plugin-registry/build/dockerfiles/rhel.install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (c) 2018-2020 Red Hat, Inc.
-# Copyright IBM Corporation 2020
+# Copyright IBM Corporation 2020-2021
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
